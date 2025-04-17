22e47c227051e323f90d12e2fa9d9a3b93c8d25a
Owen6 <owen.box2@gmail.com>
2025-04-17 09:57:15 -0700

    Fixed logic and passes all tests except for one. Original commit date should have been 4/17

--- script.js
+++ script.js
@@ -1,40 +1,41 @@
 const input = document.getElementById("user-input");
 const submitBtn = document.getElementById("check-btn");
 const clearBtn = document.getElementById("clear-btn");
+const results = document.getElementById("results-div");
+
+let testNum = 1
 
 window.onload = () => {
   input.oninput = update;
-  //input.addEventListener("input", update);
-  submitBtn.addEventListener('click', isValid)
+  submitBtn.addEventListener('click', isValid);
+  clearBtn.addEventListener('click', clear);
 }
-
 const update = event => {
   const element = event.target;
-  const value = element.value.replace(/[\s-]/g, "");
-  console.log(value);
-  //console.log("check");
+  const value = element.value.replace(/[\s-\(\)]/g, "");
+
+}
+
+const clear = () => {
+  results.textContent = "";
 }
 
 const isValid = () => {
+  const cleaned = input.value.replace(/[^\d-\(\)]/g, "");
+  console.log(cleaned + ` : ${testNum}`);
+  testNum++;
+  //leading1 ,space/dash, 3 digits either wrapped in para or not, space/dash, 3 digits, space/dash, 4 digits
+  const validRegex = /^1?[\s-]?(\(\d{3}\)|\d{3})[\s-]?\d{3}[\s-]?\d{4}$/;
   if(input.value.length <= 0) {
     alert("Please provide a phone number");
   }
-  const cleaned = input.value.replace(/[^\d]/g, "");
-  //if(cleaned.length < 7){}
-  switch(cleaned.length) {
-    case 10: 
-      console.log("valid");
-      break;
-    case 11:
-      if(cleaned[0] === "1"){
-        console.log("valid");
-      }else{
-        console.log("invalid");
-      }
-      break;
-    default:
-      console.log("invalid");
-      break;
+  if (validRegex.test(cleaned)){
+    console.log("valid");
+    results.textContent = `Valid US number: ${input.value}`;
+    return true;
+  }else{
+    console.log("invalid");
+    results.textContent = `Invalid US number: ${input.value}`;
+    return false;
   }
-  console.log(cleaned.length);
 }
