9a04518ef768788fcd9ea2d2dbda9c4f20fdaefb
Owen6 <owen.box2@gmail.com>
2025-04-18 12:54:35 -0700

    started styling following completion of logic. Need to change color depenedent on if a valid number or not

--- script.js
+++ script.js
@@ -2,6 +2,7 @@ const input = document.getElementById("user-input");
 const submitBtn = document.getElementById("check-btn");
 const clearBtn = document.getElementById("clear-btn");
 const results = document.getElementById("results-div");
+const container = document.getElementById("container");
 
 let testNum = 1
 
@@ -9,18 +10,75 @@ window.onload = () => {
   input.oninput = update;
   submitBtn.addEventListener('click', isValid);
   clearBtn.addEventListener('click', clear);
+  document.addEventListener('click', () => {
+    input.focus();
+  });
 }
+
 const update = event => {
   const element = event.target;
   const value = element.value.replace(/[\s-\(\)]/g, "");
-  //drawNumber(value);
+  drawNumber(value);
 }
 
 const drawNumber = input => {
+  const length = input.length;
+  //console.log("test")
+  container.innerHTML = '';
+  let added = false;
+  for(let i=0; i<length; i++){
+    switch(true){
+      case length >= 4 && length <= 7 && i === 3:
+        const dash = document.createElement("div");
+        dash.className = "num";
+        dash.textContent = "-";
+        container.appendChild(dash);
+        break;
+      case length > 7 && length < 12:
+        //(123)567-9012
+        if(length === 11 && added === false){
+          const number = document.createElement("div");
+          number.className = "num";
+          number.textContent = input[i];
+          container.appendChild(number);
+          input = input.slice(1);
+          added = true;
+          console.log('test');
+        }
+        if(i === 0){
+          const openPara = document.createElement("div");
+          openPara.className = "num";
+          openPara.textContent = "(";
+          container.appendChild(openPara);
+        }else if(i === 3){
+          const closedPara = document.createElement("div");
+          closedPara.className = "num";
+          closedPara.textContent = ")";
+          container.appendChild(closedPara);
+        }else if(i === 6){
+          const dash = document.createElement("div");
+          dash.className = "num";
+          dash.textContent = "-";
+          container.appendChild(dash);
+        }
+        break;
+      default:
+        break;
+    }  
+    const number = document.createElement("div");
+    number.className = "num";
+    number.textContent = input[i];
+    container.appendChild(number);
+  }
+  if(added){
+    container.removeChild(container.lastChild);
+  }
 }
 
 const clear = () => {
   results.textContent = "";
+  input.value = "";
+  update({target: input});
 }
 
 const isValid = () => {
